2673d02fc96e55338ef987fbb55d4118f5351948
henzg <zacharyghenry@gmail.com>
2025-07-22 15:15:43 -0700

    created index buffer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -120,30 +120,34 @@ int main(void)
     unsigned int startOfIndex = 0;
     unsigned int numOfIndicies = arrayPositionSize / 2;
 
-    float positions[arrayPositionSize] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f, //0
+         0.5f, -0.5f, //1
+         0.5f,  0.5f, //2
+        -0.5f,  0.5f, //3
+    };
+
+    unsigned int indicies[] =
+    {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int numOfBuffers = 1;
     unsigned int buffer;
     glGenBuffers(numOfBuffers, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, arrayPositionSize * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    unsigned int ibo; //index buffer object
+    glGenBuffers(numOfBuffers, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-    std::cout << "VERTEXT\n";
-    std::cout << source.VertexSource << "\n";
-    std::cout << "FRAGMENT\n";
-    std::cout << source.FragmentSource << "\n";
-
-
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
@@ -153,7 +157,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, startOfIndex, numOfIndicies);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
