90e822cbee5d19d4e05c6add35fd67886aafead6
henzg <zacharyghenry@gmail.com>
2025-07-23 08:31:35 -0700

    created my first uniform, square now is a disco sqaure

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -9,7 +9,7 @@
 #define ASSERT(x) if (!(x)) __debugbreak(); //note this is compiler specific
 #define GLCall(x) GLClearError();\
     x;\
-    ASSERT(GLLogCall(#x, __FILE__, __LINEjj__))
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
 
 static void GLClearError()
@@ -132,6 +132,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error!\n";
 
@@ -168,12 +170,30 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color")); // retrieve location of variable in shader
+    ASSERT(location != -1); // make sure u_Color is found
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f)); //4f = four float points, sets variable 
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
