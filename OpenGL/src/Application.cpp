a1a894e8f3f853a10aa7b484f13567545f42b94c
henzg <zacharyghenry@gmail.com>
2025-07-28 13:26:17 -0700

    added some maths for blending woot

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -14,6 +14,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -83,11 +86,14 @@ int main(void)
         // creates index buffer
         IndexBuffer ib(indicies, 6);
 
+        glm::mat4 proj = glm::ortho(-2.f, 2.f, -1.5f, 1.5f, -1.f, 1.f);
+
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", proj);
 
-        Texture texture("res/textures/lion .png");
+        Texture texture("res/textures/lion.png");
         texture.Bind();
         shader.SetUniform1i("u_Texture", 0); // 0 in this case should match the slot that the texture is bound to 
 
