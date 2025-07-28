af75e5c7d9dc805f79e7136c5fde1230be4f9812
henzg <zacharyghenry@gmail.com>
2025-07-28 08:50:08 -0700

    texture working. Was a bug in vertexarray.cpp

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -36,6 +36,7 @@ int main(void)
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
@@ -47,10 +48,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << "\n";
     {
         float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f, //0
-             0.5f, -0.5f, 1.0f, 0.0f, //1
-             0.5f,  0.5f, 1.0f, 1.0f, //2
-            -0.5f,  0.5f, 0.0f, 1.0f//3
+            -0.5f, -0.5f, 0.0f, 0.0f,   //0
+             0.5f, -0.5f, 1.0f, 0.0f,   //1
+             0.5f,  0.5f, 1.0f, 1.0f,   //2
+            -0.5f,  0.5f, 0.0f, 1.0f    //3
         };
 
         unsigned int indicies[] =
@@ -71,8 +72,8 @@ int main(void)
         VertexBuffer vb(positions, 4 * 4 * sizeof(float));
         // make vertex buffer layout
         VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
+        layout.Push<float>(2); // positions
+        layout.Push<float>(2); // texcoord
         va.AddBuffer(vb, layout);
 
         // creates index buffer
@@ -82,7 +83,7 @@ int main(void)
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
-        Texture texture("C:/dev/OpenGL/OpenGL/res/textures/lion.png");
+        Texture texture("res/textures/bird.png");
         texture.Bind();
         shader.SetUniform1i("u_Texture", 0); // 0 in this case should match the slot that the texture is bound to 
 
