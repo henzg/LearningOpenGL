1065121154ed5090ae800494e76c02335fdce6bb
henzg <zacharyghenry@gmail.com>
2025-07-28 07:54:31 -0700

    textures textures textures...kinda work kinda dont

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -12,6 +12,8 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -45,10 +47,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << "\n";
     {
         float positions[] = {
-            -0.5f, -0.5f, //0
-             0.5f, -0.5f, //1
-             0.5f,  0.5f, //2
-            -0.5f,  0.5f, //3
+            -0.5f, -0.5f, 0.0f, 0.0f, //0
+             0.5f, -0.5f, 1.0f, 0.0f, //1
+             0.5f,  0.5f, 1.0f, 1.0f, //2
+            -0.5f,  0.5f, 0.0f, 1.0f//3
         };
 
         unsigned int indicies[] =
@@ -66,9 +68,10 @@ int main(void)
         // vertex array init
         VertexArray va;
         // creates vertex buffer
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
         // make vertex buffer layout
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
@@ -78,6 +81,10 @@ int main(void)
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+        Texture texture("C:/dev/OpenGL/OpenGL/res/textureslion.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0); // 0 in this case should match the slot that the texture is bound to 
 
         // clear everything
         va.Unbind();
@@ -100,6 +107,7 @@ int main(void)
 
             //shader.Bind();
             shader.Bind();
+            texture.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
             renderer.Draw(va, ib, shader);
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -18,7 +18,7 @@ private:
 	std::unordered_map<std::string, int> m_UniformLocationCache;
 
 private: 
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 	ShaderProgramSource ParseShader(const std::string& filePath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
@@ -29,6 +29,8 @@ public:
 	~Shader();
 
 	// Set uniforms
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 	void Bind() const;
