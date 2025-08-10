bad3dbcca342a56b3d37d90d15d1a85b6ab246b8
henzg <zacharyghenry@gmail.com>
2025-08-10 09:17:15 -0700

    added translation to our rendered obj in im gui

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -91,14 +91,11 @@ int main(void)
 
         glm::mat4 proj = glm::ortho(0.f, 960.f, -0.f, 540.f, -1.f, 1.f); // example is in perpixle
         glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(-100, 0, 0)); // position of camera
-        glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(200, 200, 0));
-
-        glm::mat4 mvp = proj * view * model; // model view projection model -> view -> projection
-
+       
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", mvp);
+
 
         Texture texture("res/textures/lion.png");
         texture.Bind();
@@ -118,13 +115,11 @@ int main(void)
         ImGui::StyleColorsDark();
         ImGui_ImplOpenGL3_Init();
 
+        glm::vec3 translation(200, 200, 0);
+
         // set r variable for red color
         float r = 0.0f;
         float increment = 0.05f;
-
-		bool show_demo_window = true;
-		bool show_another_window = false;
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -135,12 +130,15 @@ int main(void)
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
-            ImGui::ShowDemoWindow();
+
+            glm::mat4 model = glm::translate(glm::mat4(1.f), translation);
+			glm::mat4 mvp = proj * view * model;
 
             //shader.Bind();
             shader.Bind();
             texture.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(va, ib, shader);
 
@@ -150,28 +148,10 @@ int main(void)
                 increment = 0.05f;
 
             r += increment;
-            // sample imgui code
-			//{
-			//	static float f = 0.0f;
-			//	static int counter = 0;
-
-			//	ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-			//	ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			//	ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-			//	ImGui::Checkbox("Another Window", &show_another_window);
-
-			//	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			//	ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			//	if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-			//		counter++;
-			//	ImGui::SameLine();
-			//	ImGui::Text("counter = %d", counter);
-
-			//	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-			//	ImGui::End();
-			//}
+            {
+                ImGui::SliderFloat3("float", &translation.x, 0.f, 960.f);
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            }
 
             // imgui rendering
             ImGui::Render();
